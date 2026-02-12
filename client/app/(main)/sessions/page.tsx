c4b67a5831b38634fd36b35212c5a9abdf5caa8f
Lokesh <lokeshnegi649@gmail.com>
2026-02-12 12:05:34 +0530

    feat: add sessions section for navbar

---
+++ client/app/(main)/sessions/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sessions from '../_components/Sessions';
+
+const page = () => {
+  return (
+    <div>
+      <Sessions />
+    </div>
+  );
+};
+
+export default page;
