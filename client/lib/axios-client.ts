36ec9ccda42782529f41ebe296c7763013c53638
Lokesh <lokeshnegi649@gmail.com>
2026-02-13 14:29:03 +0530

    fix: deployment fixes for same site cookies

--- client/lib/axios-client.ts
+++ client/lib/axios-client.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const options = {
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000/api',
+  baseURL:
+    process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000/api/v1',
   withCredentials: true,
   timeout: 10000,
 };
